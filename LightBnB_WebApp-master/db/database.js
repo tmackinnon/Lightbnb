274fd44c04af295642d45edd645a3d4c9429ffb0
Tara MacKinnon <taramackinnon4@gmail.com>
2023-04-07 01:39:19 +0000

    Update addProperty function to save a new property to the properties table

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -9,8 +9,6 @@ const pool = new Pool({
   database: 'lightbnb'
 });
 
-//email in db: tristanjacobs@gmail.com
-
 /// Users
 
 /**
@@ -59,7 +57,6 @@ const addUser = function(user) {
   return pool
     .query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *;`, queryParams)
     .then((result) => {
-      // console.log(result.rows[0]);
       return result.rows[0];
     })
     .catch((err) => {
@@ -168,10 +165,22 @@ const getAllProperties = function(options, limit = 10) {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const costInCents = property.cost_per_night * 100;
+  const queryParams = [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, costInCents, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.country, property.street, property.city, property.province, property.post_code];
+
+  const queryString = `
+  INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) 
+  RETURNING *;`;
+
+  return pool
+    .query(queryString, queryParams)
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 module.exports = {
